b5ca476659c74098afb7721c543433a343b10731
Zazelus <mansour091800@gmail.com>
2022-07-23 17:42:05 -0400

    Created data, entity, generator, repository, and service packages. Created AbstractEntity class and added some boilerplate to ListView.java.

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,19 +1,35 @@
 package com.example.application.views.list;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@PageTitle("list")
+@PageTitle("Contacts | CL Dashboard")
 @Route(value = "")
 public class ListView extends VerticalLayout {
 
+    Grid<Contact> grid = new Grid<>(Contact.class);
+    TextField filterText = new TextField();
+
     public ListView() {
-        add(new H1("Hello world!"));
+        addClassName("list-view");
+        setSizeFull();
+
+        configureGrid();
+    }
+
+    /**
+     * Sets some attributes for our contact-containing grid.
+     */
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
     }
 
 }
